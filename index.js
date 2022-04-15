f5f8d538b1bf38a4f9477ee43a7852d0c26687c2
MarcoCondinanzo <marcocondinanzo@gmail.com>
2022-04-14 23:56:03 -0300

    puerto heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const app=require('./app');
 
-const port = 3000; 
+const port = process.env.PORT| 3000; 
 
 app.listen(port, ()=> {
     'Escuchando en el puerto 3000'
